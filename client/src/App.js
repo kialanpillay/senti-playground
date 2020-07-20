3bc655547d57beba71aacf40fa1b52295e1de89e
Kialan Pillay <kialanpillay@gmail.com>
2020-07-20 12:25:49 +0200

    AWS Amplify Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Amplify from 'aws-amplify';
+import Predictions, { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.awsPrediction = this.awsPrediction.bind(this);
+  }
+
+  awsPrediction = () => {
+    Predictions.interpret({
+      text: {
+        source: {
+          text: "Hello this is a test.",
+        },
+        type: "ALL"
+      }
+    })
+    .then(result => console.log({ result }))
+    .catch(err => console.log({ err }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1 className="title">SENTI</h1>
+          <h3 className="tag">sentiment analysis playground</h3>
+        </header>
+        <Container></Container>
+      </div>
+    );
+  }
 }
-
-export default App;
