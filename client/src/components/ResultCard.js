bce10a9b85e12230d82624f96cdf576df593c601
Kialan Pillay <kialanpillay@gmail.com>
2020-07-21 23:30:46 +0200

    Ignore Warning

--- client/src/components/ResultCard.js
+++ client/src/components/ResultCard.js
@@ -3,6 +3,7 @@ import Card from "react-bootstrap/Card";
 
 import Speech from "react-speech";
 
+// eslint-disable-next-line
 String.prototype.toSentenceCase = function () {
   return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
