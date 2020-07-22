56b5966b4f148e09c7d19802b6aac4c09896daeb
Kialan Pillay <kialanpillay@gmail.com>
2020-07-22 22:53:00 +0200

    Corpus Input UI

---
+++ client/src/assets/words.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    text: "hectic",
+    value: 1,
+  },
+  {
+    text: "karen",
+    value: 1,
+  },
+  {
+    text: "finna",
+    value: 1,
+  },
+  {
+    text: "boujee",
+    value: 1,
+  },
+  {
+    text: "flex",
+    value: 1,
+  },
+  {
+    text: "gucci",
+    value: 1,
+  },
+];
--- client/src/pages/Corpus.js
+++ client/src/pages/Corpus.js
@@ -2,17 +2,20 @@ import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
+import Card from "react-bootstrap/Card";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Amplify, { Auth, Hub } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import awsconfig from "../aws-exports";
+import ReactWordcloud from "react-wordcloud";
 
 import Navigation from "../components/Navigation";
+import words from "../assets/words";
 
 Amplify.configure(awsconfig);
 
@@ -26,40 +29,56 @@ const listener = (data) => {
 Hub.listen("auth", listener);
 const api = "https://senti-ment-api.herokuapp.com/";
 
+const options = {
+  colors: ["silver", "black", "gray"],
+  deterministic: true,
+  fontFamily: "Arial",
+  fontSizes: [60, 60],
+  padding: 1,
+  rotations: 2,
+  rotationAngles: [0, 90],
+  scale: "sqrt",
+  spiral: "archimedean",
+  transitionDuration: 1000,
+};
+
 class Corpus extends Component {
   constructor(props) {
     super(props);
     this.state = {
       username: "",
       auth: false,
-      phrase: "TestJS",
-      sentiment: "Negative"
+      phrase: "",
+      sentiment: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.setSentiment = this.setSentiment.bind(this);
   }
 
+  setSentiment = (value) => {
+    this.setState({ sentiment: value });
+  };
+
   handleChange = (event) => {
-    this.setState({ text: event.target.value, req: false });
+    this.setState({ phrase: event.target.value });
   };
 
   handleSubmit = () => {
     const data = {
-        user: this.state.username,
-        phrase: this.state.phrase,
-        sentiment: this.state.sentiment,
-    }
-    console.log(data)
+      user: this.state.username,
+      phrase: this.state.phrase,
+      sentiment: this.state.sentiment,
+    };
     let url = `${api}corpus`;
     fetch(url, {
       method: "PUT",
       headers: {
         "content-type": "application/json",
       },
-     body: JSON.stringify(data)
-  
-    })
+      body: JSON.stringify(data),
+    });
   };
 
   async componentDidMount() {
@@ -74,7 +93,9 @@ class Corpus extends Component {
         <Container style={{ marginTop: "4rem" }}>
           <Row>
             <Col md={10}>
-              <h1 className="heading" onClick={() => this.handleSubmit()}>The Corpus Project</h1>
+              <h1 className="heading" onClick={() => this.handleSubmit()}>
+                The Corpus Project
+              </h1>
             </Col>
           </Row>
           <Row style={{ marginTop: "0rem" }}>
@@ -85,27 +106,45 @@ class Corpus extends Component {
             </Col>
           </Row>
           <Row style={{ marginTop: "3rem" }}>
-            <Col md={10}>
+            <Col md={2}>
+              <ListGroup>
+                <ListGroup.Item
+                  action
+                  onClick={() => this.setSentiment("Positive")}
+                >
+                  Positive
+                </ListGroup.Item>
+                <ListGroup.Item
+                  action
+                  onClick={() => this.setSentiment("Negative")}
+                >
+                  Negative
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col md={6}>
               <InputGroup className="mb-3" size="lg">
                 <FormControl
-                  placeholder="Your text here"
+                  placeholder="Your phrase here"
                   onChange={this.handleChange}
                 />
-                <DropdownButton
-                  as={InputGroup.Append}
-                  variant="outline-secondary"
-                  title="Analyse"
-                  id="input-group-dropdown-2"
-                  disabled={this.state.text === ""}
-                >
-                  <Dropdown.Item onClick={() => this.handleAnalysis("bayes/")}>
-                    Senti Naive Bayes
-                  </Dropdown.Item>
-                  <Dropdown.Item onClick={() => this.handleAnalysis("vader/")}>
-                    Senti VADER
-                  </Dropdown.Item>
-                </DropdownButton>
+                <InputGroup.Append>
+                  <Button
+                    variant="outline-secondary"
+                    disabled={
+                      this.state.sentiment === "" || this.state.text === ""
+                    }
+                    onClick={() => this.handleSubmit()}
+                  >
+                    Submit
+                  </Button>
+                </InputGroup.Append>
               </InputGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={10}>
+              <ReactWordcloud options={options} words={words} />
             </Col>
           </Row>
         </Container>
